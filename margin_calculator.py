a0d8bbf8a86ec02139f0617fce94ae4e07cd9186
Ben Grunfeld <benjamin.grunfeld@gmail.com>
2013-07-16 21:17:23 -0600

    Updated: margin calculator comments

--- margin_calculator.py
+++ margin_calculator.py
@@ -8,7 +8,7 @@
 
 pageWidth=1080		#Input the page width
 numberOfCols=3		#Input how many colums you want
-margin=30			#Input how much space you think is good
+margin=30		#Input how much space you think is good
 
 if((pageWidth-(margin * (numberOfCols - 1))) % numberOfCols) == 0:
 	cols = (pageWidth-(margin*(numberOfCols-1)))/numberOfCols
