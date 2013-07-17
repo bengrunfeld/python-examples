2917d3fe566c2dbac34749fba49a73ad39c18653
Ben Grunfeld <benjamin.grunfeld@gmail.com>
2013-07-16 22:06:10 -0600

    Updated: margin calculator documentation string

--- margin_calculator.py
+++ margin_calculator.py
@@ -2,10 +2,11 @@
 
 """Calculates how wide floating columns should be in a set page width.
 
-You input the page width you're dealing with,
-how many columns you'd like to create, and what the space should 
-be between the columns, and the program calculates if you can create
-columns of equal width. If you can't, it issues an error message."""
+You input the page width you're dealing with, how many columns you'd 
+like to create, and what the space should be between the columns, and 
+the program calculates if you can create columns of equal width and 
+what that width should be. If you can't create colums of equal width, 
+it issues an error message."""
 
 
 pageWidth=1080		#Input the page width
