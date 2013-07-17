9a9521082a84076c488bfb56ac000d1b48b30204
Ben Grunfeld <benjamin.grunfeld@gmail.com>
2013-07-16 21:11:44 -0600

    Updated: Comments in margin calculator

--- margin_calculator.py
+++ margin_calculator.py
@@ -1,5 +1,10 @@
 #! /usr/bin/python
 
+#Program Description: You input the page width you're dealing with,
+#how many columns you'd like to create, and what the space should 
+#be between the columns, and the program calculates if you can create
+#columns of equal width. If you can't, it issues an error message.
+
 
 pageWidth=1080		#Input the page width
 numberOfCols=30		#Input how many colums you want
